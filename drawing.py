d2ad4cadb4139fc657868bcaa1db94f7e03194e7
Jack Grahl <jack@prismfp.com>
2018-09-12 16:46:24 +0100

    Fix staggered lines with correct maxing.

--- drawing.py
+++ drawing.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import randint, choice
 
 
 def draw_line(a, b, color="black"):
@@ -18,18 +18,30 @@ def draw_contour(contour):
 
 
 def line_max(a, b):
-    a1 = next(a, (0, 0))
-    b1 = next(b, (0, 0))
+    a = ( A for A in a )
+    b = ( B for B in b )
     a_over = b_over = False
+    a1 = b1 = (0, 0)
+    ay = by = 0
+
+    try:
+        a1 = next(a)
+    except StopIteration:
+        a_over = True
+    try:
+        b1 = next(b)
+    except StopIteration:
+        b_over = True
+
     while not (a_over and b_over):
-        if (not a_over and a1[0] <= b1[0]) or b_over:
+        if not a_over and (a1[0] <= b1[0] or b_over):
             ay = a1[1]
             x = a1[0]
             try:
                 a1 = next(a)
             except StopIteration:
                 a_over = True
-        if (not b_over and b1[0] <= a1[0]) or a_over:
+        if not b_over and (b1[0] <= a1[0] or a_over):
             by = b1[1]
             x = b1[0]
             try:
@@ -39,9 +51,9 @@ def line_max(a, b):
         yield (x, max(ay, by))
 
 
-def create_contour(xmax = 100):
+def create_contour(level=0, xmax = 100):
     x = 0
-    y = 0
+    y = level
     while x < xmax:
         yield (x, y)
         x = x + 5 + randint(0, 10)
@@ -50,10 +62,9 @@ def create_contour(xmax = 100):
 
 
 if __name__ == '__main__':
-    m = [].__iter__()
+    m = []
     for i in range(10):
-        print(i)
-        f = create_contour()
-        m = line_max(f, m)
+        f = list(create_contour(i * 5))
+        m = list(line_max(f, m))
         for line in draw_contour(m):
             print draw_line(*line)
